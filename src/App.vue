d870273e16a1b470f8885b409863dcec011e2bf2
Michael Brooks <me@michaelbrooks.co.uk>
2019-07-15 17:01:35 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="flex bounceIn">
+      <img class="mx-auto my-5" src="./assets/images/MBD_medium.png" />
+    </div>
+    <div id="hero-image-1" class="flex text-center fadeIn">
+      <div class="py-64 px-65 m-auto background-overlay">
+        <p class="text-white text-xl uppercase mt-5">Good Afternoon, I am</p>
+        <h1 class="text-white text-6xl">Michael Brooks Developments LTD</h1>
+        <p class="text-white text-xl">Creating functional websites</p>
+        <span class="divider px-20 py-6"></span>
+        <br><br><br><br>
+        <a href="#contact" class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-20 uppercase">
+          Have a project in mind? Let's talk
+        </a>
+      </div>
+    </div>
+    <div class="flex text-center fadeIn">
+      <div class="py-64 px-65 m-auto">
+        <p class="text-xl uppercase mt-5">I focus on one thing</p>
+        <h1 class="text-5xl font-bold">Making your website work for you</h1>
+      </div>
+    </div>
+    <div id="contact">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
