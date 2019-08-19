6fe7e0177d3f3a634c5a71ad6e2ac086eb1e0f6f
Michael Brooks <me@michaelbrooks.co.uk>
2019-08-19 18:30:01 +0100

    More changes to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,91 @@
 <template>
   <div id="app">
-    <div class="flex bounceIn">
-      <img class="mx-auto my-5" src="./assets/images/MBD_medium.png" />
+    <div class="md:flex bounceIn">
+      <img class="mx-auto my-5 px-2" src="./assets/images/MBD_medium.png" />
     </div>
-    <div id="hero-image-1" class="flex text-center fadeIn">
-      <div class="py-64 px-65 m-auto background-overlay">
-        <p class="text-white text-xl uppercase mt-5">Good Afternoon, I am</p>
-        <h1 class="text-white text-6xl">Michael Brooks Developments LTD</h1>
-        <p class="text-white text-xl">Creating functional websites</p>
-        <span class="divider px-20 py-6"></span>
-        <br><br><br><br>
-        <a href="#contact" class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-20 uppercase">
-          Have a project in mind? Let's talk
-        </a>
+
+    <div id="hero-image-1" class="md:flex text-center fadeIn">
+      <div class="py-64 px-65 w-full m-auto background-overlay">
+        <p class="text-white text-l uppercase mt-5">Good Afternoon, I am</p>
+        <h1 class="text-white text-5xl">Michael Brooks Developments LTD</h1>
+        <p class="text-white text-3xl">Creating functional websites</p>
+
+        <span class="divider px-6 py-6 mb-20"></span>
+
+        <div class="w-full mt-24">
+          <a href="#contact" class="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-4 uppercase">
+            Have a project in mind? Let's talk
+          </a>
+        </div>
       </div>
     </div>
-    <div class="flex text-center fadeIn">
-      <div class="py-64 px-65 m-auto">
-        <p class="text-xl uppercase mt-5">I focus on one thing</p>
-        <h1 class="text-5xl font-bold">Making your website work for you</h1>
+    <div class="md:flex mt-16 text-center fadeIn">
+      <div class="pt-20 pb-1 m-auto inline">
+        <p class="text-l uppercase mt-5 mb-4">I focus on one thing</p>
+        <h1 class="text-3xl px-2 font-bold">Making your website work for you</h1>
+      </div>
+    </div>
+    <div class="md:flex text-center fadeIn pb-32 mt-4">
+      <div class="w-1/2">
+
+      </div>
+      <div class="md:flex w-1/1">
+        <div class="md:w-1/4 mr-6 mb-6 px-6">
+          <img src="https://michaelbrooks.dev/wp-content/uploads/2019/03/brooks.png" alt="">
+          <p class="mt-4 text-2xl">Brooks Pet Sitters</p>
+          <p class="text-gray-600">
+            I helped my wife start her own business, and created her website with functional design and SEO in mind.
+          </p>
+        </div>
+        <div class="md:w-1/4 mr-12 mb-6 px-6">
+          <img src="https://michaelbrooks.dev/wp-content/uploads/2019/03/logo-new-black.jpg" alt="">
+          <p class="mt-4 text-2xl">My Haulfryn</p>
+          <p class="text-gray-600">
+            Built entirely with Laravel, this website enabled holiday and residential homeowners the ability to pay for
+            their bills. They can log in and pay for electric and gas, as well as add visitor registration numbers to
+            allow them to enter.
+          </p>
+        </div>
+        <div class="md:w-1/4 px-6">
+          <img src="https://michaelbrooks.dev/wp-content/uploads/2019/03/pulse8communications.jpg" alt="">
+          <p class="mt-4 text-2xl">Pulse8 Communications</p>
+          <p class="text-gray-600">
+            Customers can view broadband plans and sign up to their chosen plan. Pulse8 Broadband can then log into
+            their system and view purchases, support tickets and how far the installation process has gone.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="md:flex md:py-32 w-full m-auto bg-black">
+      <div class="md:w-1/5"></div>
+      <div class="w-full md:w-1/3">
+        <img src="https://michaelbrooks.dev/wp-content/uploads/2019/03/img1.jpg" alt="">
+      </div>
+      <div class="w-full mt-6 md:w-1/4 text-center md:text-left">
+        <p class="text-white text-5xl">About Me</p>
+        <span class="divider px-4"></span>
+        <p class="text-white mt-8 px-6 md:px-0">
+          I’ve been in the professional trade for over eight years. I’m a master of a select number of programming languages including; HTML, CSS, and PHP to name a few.
+        </p>
+        <p class="text-white mt-8 px-6 md:px-0">
+          I take pride in my work with much attention to detail. Please take a look at the projects above to see examples of my work.
+        </p>
+        <p class="text-white mt-8 px-6 md:px-0">
+          Over the past eight years, I have been growing my skill set which provides my client base with the most up to date technologies available.
+        </p>
+        <p class="text-white mt-8 px-6 md:px-0">
+          I provide all of this at affordable pricing without cutting corners. I help with merging conflicts and provide superior support and advice when required.
+        </p>
+        <p class="text-white mt-8 px-6 md:px-0">
+          <a href="#contact">Contact me today!</a>
+        </p>
+        <div class="mt-8 px-6 md:px-0">
+          <div class="elementor-social-icons-wrapper">
+            <a href="https://www.fb.me/michaelbrooksdevelopments/" class="elementor-icon elementor-social-icon elementor-social-icon-facebook elementor-repeater-item-fxed6qd" target="_blank">
+              <span class="elementor-screen-only">Facebook</span> <i class="fa fa-facebook text-xl"></i>
+            </a>
+            <a href="https://twitter.com/mike_d_brooks" class="elementor-icon elementor-social-icon elementor-social-icon-twitter elementor-repeater-item-32q8bqx" target="_blank"> <span class="elementor-screen-only">Twitter</span> <i class="fa fa-twitter"></i> </a> <a href="https://www.linkedin.com/company/michael-brooks-developments" class="elementor-icon elementor-social-icon elementor-social-icon-linkedin elementor-repeater-item-d6pdnr9" target="_blank"> <span class="elementor-screen-only">Linkedin</span> <i class="fa fa-linkedin"></i> </a> <a href="https://github.com/Michael-Brooks" class="elementor-icon elementor-social-icon elementor-social-icon-github elementor-repeater-item-3ea3f99" target="_blank"> <span class="elementor-screen-only">Github</span> <i class="fa fa-github"></i> </a></div>
+        </div>
       </div>
     </div>
     <div id="contact">
