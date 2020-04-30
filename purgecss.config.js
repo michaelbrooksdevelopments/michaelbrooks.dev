778d9930e0c6701b32a303bbe97afe086f2ac6e4
Michael Brooks <me@michaelbrooks.co.uk>
2020-04-30 10:23:25 +0100

    Added fontawesome to purgecss whitelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -15,7 +15,9 @@ module.exports = {
         'g-image',
         'g-image--lazy',
         'g-image--loaded',
+        'svg-inline--fa',
     ],
+    whitelistPatterns: [/shiki/, /fa-$/],
     extractors: [
         {
             extractor: content => content.match(/[A-z0-9-:\\/]+/g),
