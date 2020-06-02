0c3ef2a6a63f8816b9c869deee6f3a5c855fa3e1
Michael Brooks <me@michaelbrooks.co.uk>
2020-06-02 11:30:23 +0100

    Added favicon icons for all devices

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,46 +2,57 @@ const tailwind = require('tailwindcss')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 const postcssPlugins = [
-  tailwind(),
+    tailwind(),
 ]
 
 if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
-  siteName: 'Michael Brooks Developments',
-  plugins: [
-    {
-      use: 'gridsome-plugin-matomo',
-      options: {
-        host: 'https://analytics.michaelbrooks.dev',
-        siteId: 2
-      }
+    siteName: 'Michael Brooks Developments',
+    icon: {
+        favicon: {
+            src: './src/favicons/favicon.png',
+            sizes: [16, 32, 96]
+        },
+        touchicon: {
+            src: './src/favicons/apple-icon.png',
+            sizes: [57, 60, 72, 76, 114, 120, 144, 152, 180],
+            precomposed: true
+        }
     },
-    {
-        use: 'gridsome-plugin-pwa',
-        options: {
-            title: 'Michael Brooks Developments',
-            startUrl: '/',
-            display: 'standalone',
-            statusBarStyle: 'default',
-            manifestPath: 'manifest.json',
-            disableServiceWorker: false,
-            serviceWorkerPath: 'service-worker.js',
-            cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
-            shortName: 'MB Developments',
-            themeColor: '#4FD1C5',
-            backgroundColor: '#ffffff',
-            icon: 'src/favicon.png',
-            msTileImage: '',
-            msTileColor: '#4FD1C5'
-          }
-      }
-  ],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
+    plugins: [
+        {
+            use: 'gridsome-plugin-matomo',
+            options: {
+                host: 'https://analytics.michaelbrooks.dev',
+                siteId: 2
+            }
+        },
+        {
+            use: 'gridsome-plugin-pwa',
+            options: {
+                title: 'Michael Brooks Developments',
+                startUrl: '/',
+                display: 'standalone',
+                statusBarStyle: 'default',
+                manifestPath: 'manifest.json',
+                disableServiceWorker: false,
+                serviceWorkerPath: 'service-worker.js',
+                cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+                shortName: 'MB Developments',
+                themeColor: '#4FD1C5',
+                backgroundColor: '#ffffff',
+                icon: 'src/favicon.png',
+                msTileImage: './favicons/ms-icon-70x70.png',
+                msTileColor: '#4FD1C5'
+            }
+        }
+    ],
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: postcssPlugins,
+            },
+        },
     },
-  },
 }
